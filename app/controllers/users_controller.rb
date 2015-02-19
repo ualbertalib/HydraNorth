091888663d8dbd6d76dc50a595d296143e865b88
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-02-18 17:20:24 -0700

    add lockable so an administrator can lock malicious use of accounts
    add edit profile for adminstrators
    add activate/deactivate
    add user and groups info to edit page
    add edit user name
    tests feature, controller and model
    fixes #60
    
     * user model has attributes to lock and unlock
     * feature for admin to lock and unlock is present
     * locked out users are locked out
     * non-admin cannot lock and unlock
     * test that controller redirects to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   include Hydranorth::UsersControllerBehavior
 
+  def edit
+    @user = User.from_url_component(params[:id])
+    @trophies = @user.trophy_files
+  end
+
   def user_is_current_user
     redirect_to sufia.profile_path(@user.to_param), alert: "Permission denied: cannot access this page." unless @user == current_user || current_user.admin?
   end
+
+  def lock_access
+    @user.lock_access! unless ! current_user.admin?
+    redirect_to sufia.profile_path(@user.to_param)
+  end
+
+  def unlock_access
+    @user.unlock_access! unless ! current_user.admin?
+    redirect_to sufia.profile_path(@user.to_param)
+  end
+
 end
