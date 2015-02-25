499a00f6c71c747455511ce3885b0bfe0e9d0aa8
criedlberger <christian.riedlberger@ualberta.ca>
2015-02-25 13:35:53 -0700

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,29 @@ Hydranorth::Application.routes.draw do
   get 'users/:id/unlock_access' => 'users#unlock_access', as: 'unlock_access_user'
 
   root to: "homepage#index"
+  
+  # Routes for user's files, collections, highlights and shares
+  # Preserves existing behavior by maintaining paths to /dashboard
+  # Routes actions to the various My controllers
+  scope :dashboard do
+    get '/files',             controller: 'my/files', action: :index, as: 'dashboard_files'
+    get '/files/page/:page',  controller: 'my/files', action: :index
+    get '/files/facet/:id',   controller: 'my/files', action: :facet, as: 'dashboard_files_facet'
+
+    get '/all',             controller: 'my/all', action: :index, as: 'dashboard_all'
+    get '/all/page/:page',  controller: 'my/all', action: :index
+    get '/all/facet/:id',   controller: 'my/all', action: :facet, as: 'dashboard_all_facet'
+    
+    get '/collections',             controller: 'my/collections', action: :index, as: 'dashboard_collections'
+    get '/collections/page/:page',  controller: 'my/collections', action: :index
+    get '/collections/facet/:id',   controller: 'my/collections', action: :facet, as: 'dashboard_collections_facet'
+
+    get '/highlights',            controller: 'my/highlights', action: :index, as: 'dashboard_highlights'
+    get '/highlights/page/:page', controller: 'my/highlights', action: :index
+    get '/highlights/facet/:id',  controller: 'my/highlights', action: :facet, as: 'dashboard_highlights_facet'
+
+    get '/shares',            controller: 'my/shares', action: :index, as: 'dashboard_shares'
+    get '/shares/page/:page', controller: 'my/shares', action: :index
+    get '/shares/facet/:id',  controller: 'my/shares', action: :facet, as: 'dashboard_shares_facet'
+  end
 end
