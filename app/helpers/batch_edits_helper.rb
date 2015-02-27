7966526a9d335ab48036395ca3fd6a463a5f3f75
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-02-27 16:56:21 -0700

    non-admin users cannot destroy GenericFile fixes #33
    also removed the batch delete button (batchuser can still delete)
    also added unauthorized message so user knows to contact admin
    
    also removed the batch delete button (batchuser can still delete)
    also added unauthorized message so user knows to contact admin
    
    also removed the batch delete button (batchuser can still delete)
    also added unauthorized message so user knows to contact admin

---
+++ app/helpers/batch_edits_helper.rb
@@ -0,0 +1,13 @@
+# View Helpers for Hydra Batch Edit functionality
+module BatchEditsHelper
+  # Displays the delete button for batch editing
+  def batch_delete
+    render partial: '/batch_edits/delete_selected' if current_user.admin?
+  end
+
+  def render_check_all
+    unless params[:controller].match("my/collections")
+      render partial: 'batch_edits/check_all'
+    end
+  end
+end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
   include Sufia::Ability
 
   def custom_permissions 
+    cannot :destroy, ::GenericFile unless user_groups.include? 'admin'
     can :manage, :all if user_groups.include? 'admin'
   end
   
