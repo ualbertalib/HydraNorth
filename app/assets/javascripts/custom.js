fb4e50dee05b7f3e6a357b0c75b7a35cf44f3bfb
Natasha Nunn <natasha@nnunn.ca>
2015-04-09 12:09:17 -0600

    adding twitter widget and more styling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -22,4 +22,44 @@ $(document).ready(function(){
  	$('#slide1').css({'background-image': 'url(assets/' + images[Math.floor(Math.random() * images.length)] + ')'});
  	var images2 = ['slide2.jpg', 'slide3.jpg', 'slide4.jpg', 'slide7.jpg'];
  	$('#slide2').css({'background-image': 'url(assets/' + images2[Math.floor(Math.random() * images.length)] + ')'});
+
+ 	var config1 = {
+  		"id": '586195044660424704',
+  		"domId": 'tweets',
+  		"maxTweets": 2,
+  		"enableLinks": true
+	};
+	twitterFetcher.fetch(config1);
+	
+	var doughnutData = [
+				{
+					value: 1,
+					label: "One"
+				},
+				{
+					value: 2,
+					label: "Two"
+				},
+				{
+					value: 3,
+					label: "Three"
+				},
+				{
+					value: 4,
+					label: "Four"
+				},
+				{
+					value: 5,
+					label: "Five"
+				}
+
+			];
+
+			window.onload = function(){
+				var ctx = document.getElementById("chart-area").getContext("2d");
+				window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+			};
+
+
+
 });
