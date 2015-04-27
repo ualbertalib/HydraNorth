bcb30987c1f19f5184e3b10fbc881e11a0d75d83
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-04-27 15:18:53 -0600

    added feature tests for admin_resource_type_list

--- spec/features/admin_resource_types_spec.rb
+++ spec/features/admin_resource_types_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
-describe 'admin resource types', :type => :feature do
+describe 'resource_types_list', :type => :feature do
 
   let(:user) { FactoryGirl.create :user_with_fixtures }
   let(:admin) { FactoryGirl.create :admin }
-  let!(:file) do
+  let!(:file1) do
     GenericFile.new.tap do |f|
       f.title = ['little_file.txt']
       f.creator = ['little_file.txt_creator']
       f.resource_type = ["stuff" ]
       f.read_groups = ['public']
       f.apply_depositor_metadata(user.user_key)
+      f.save!
+    end
+  end
+  let!(:file2) do
+    GenericFile.new.tap do |f|
+      f.title = ['little_file.txt']
+      f.creator = ['little_file.txt_creator']
+      f.resource_type = ["stuff" ]
+      f.read_groups = ['public']
+      f.apply_depositor_metadata(admin.user_key)
       f.save!
     end
   end
@@ -23,13 +33,13 @@ describe 'admin resource types', :type => :feature do
     before do 
       sign_in admin
       visit "/dashboard/files"
-      within("#document_#{file.id}") do
+      within("#document_#{file2.id}") do
         click_button "Select an action"
         click_link "Edit File"
       end
     end
     it 'has admin resource list' do
-      page.should have_select('resource_types', :options => Sufia.config.admin_resource_types)
+      expect(page).to have_select('generic_file_resource_type', :options => ["Book","Book Chapter", "Computing Science Technical Report", "Conference/Workshop Poster","Conference/Workshop Presentation", "Dataset", "Image", "Journal Article (Draft-Submitted)", "Journal Article (Published)", "Learning Object", "Report", "Research Material", "Review", "Structural Engineering Report", "Thesis"])
     end
 
   end
@@ -38,13 +48,14 @@ describe 'admin resource types', :type => :feature do
     before do
       sign_in user
       visit "/dashboard/files"
-      within("#document_#{file.id}") do
+      within("#document_#{file1.id}") do
         click_button "Select an action"
         click_link "Edit File"
       end
     end
     it 'has regular resource list' do
-      page.should have_select('resource_types', :options => Sufia.config.resource_types)
+      expect(page).to have_select('generic_file_resource_type', :options => ["Book", "Book Chapter", "Conference/Workshop Poster", "Conference/Workshop Presentation", "Dataset", "Image","Journal Article (Draft-Submitted)", "Journal Article (Published)", "Learning Object", "Report", "Research Material", "Review"])
+
     end
 
   end
