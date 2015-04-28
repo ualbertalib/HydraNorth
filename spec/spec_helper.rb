f65829f4b306f34cef4cd62f3be9ffddf2b1be73
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-04-28 16:38:01 -0600

    use show_me_the_cookies to assert owasp requirement to invalidate cookies on logout  * functionality handled by devise  * resolves #190

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,6 +50,8 @@ RSpec.configure do |config|
 
   config.fixture_path = File.expand_path("../fixtures", __FILE__)
 
+  config.include ShowMeTheCookies, :type => :feature
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
