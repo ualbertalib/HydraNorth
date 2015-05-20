ff61699ef6600230cc0ae6321b096e3ccd8ca3c0
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-05-20 11:47:49 -0600

    display date_created instead of date_uploaded on the brief record of search results fixes #270

--- app/models/concerns/hydranorth/solr_document_behavior.rb
+++ app/models/concerns/hydranorth/solr_document_behavior.rb
@@ -8,5 +8,13 @@ module Hydranorth
       Array(self[Solrizer.solr_name("subject")])
     end
 
+    def date_created
+      Array(self[Solrizer.solr_name("date_created")]).first
+    end
+
+    def date_created?
+      !self[Solrizer.solr_name("date_created")].nil?
+    end
+
   end
 end
