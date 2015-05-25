59d988c82d0e0e1e7bfc067953f958b278b4e409
nnunn <natasha@nnunn.ca>
2015-05-25 15:32:46 -0600

    hack to get the urls to change. Unfortunately I could not figure out how to do this within Rails so I used javascript. Seems to work well and should be enough for the soft launch

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -1,3 +1,15 @@
 module BrowseHelper
-  include Blacklight::BrowseHelperBehavior
+
+	include Blacklight::Facet
+   
+ def has_browse_values? fields = facet_field_names, options = {}
+    facets_from_request(fields).any? { |display_facet| !display_facet.items.empty? }
+  end
+
+   def render_browse_value(facet_field, item, options ={})
+    path = search_action_path(add_facet_params_and_redirect(facet_field, item))
+    content_tag(:span, :class => "facet-label") do
+      link_to_unless(options[:suppress_link], facet_display_value(facet_field, item), path, :class=>"facet_select")
+    end + render_facet_count(item.hits)
+  end
 end
