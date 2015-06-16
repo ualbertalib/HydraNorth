7cc42ad04d2d0726efd969b088f920717f00f695
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-06-16 15:52:19 -0600

    change upload sizes to fix #253

---
+++ app/assets/javascripts/hydranorth_fileupload.js
@@ -0,0 +1,6 @@
+//1 GB max file size 
+max_file_size = 1000000000;
+max_file_size_str = "1 GB";
+//1.5 GB max total upload size
+max_total_file_size = 1500000000;
+max_total_file_size_str = "1.5 GB";
