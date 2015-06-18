faecd47f7fa809fa3b402218a5012a71ed7010e2
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-06-18 16:19:24 -0600

    add ability to link CCID credentials to existing account

--- spec/features/saml_spec.rb
+++ spec/features/saml_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
 
 describe 'SAML' do
-  before do
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:shibboleth] = OmniAuth::AuthHash.new({
-      :provider => 'shibboleth',
-      :eppn => 'myself@testshib.org',
-      :uid => 'myself@testshib.org'
-    })
-  end
 
   it { expect { visit '/users/sign_in' }.to_not raise_error }
 
-  it 'should use SAML to create acount' do
-    visit '/users/sign_in'
-    expect { click_link "Sign in with Shibboleth" }.to_not raise_error
-    expect(page).to have_content "Successfully authenticated from Shibboleth account."
-    expect(current_path).to eq('/dashboard')
+
+  describe 'should be able to use SAML credential to login' do
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:shibboleth] = OmniAuth::AuthHash.new({
+        :provider => 'shibboleth',
+        :eppn => 'myself@testshib.org',
+        :uid => 'myself@testshib.org'
+      })
+    end
+
+    it 'should use SAML to create acount' do
+      visit '/users/sign_in'
+      expect { click_link "Sign in with Shibboleth" }.to_not raise_error
+      expect(page).to have_content "Successfully authenticated from Shibboleth account."
+      expect(current_path).to eq('/dashboard')
+    end
+  end
+
+  describe 'should be able to link SAML credentials to existing account' do
+    let(:user) { FactoryGirl.create :user }
+    after :all do
+      cleanup_jetty
+    end
+
+    before do
+      sign_in user
+      click_link "Edit Profile"
+    end
+
+    it { expect(page).to have_content('Link CCID credentials to account') }
   end
 end
