d606bc7797db92267feac071e33f07969d309052
Peter Binkley <peter.binkley@ualberta.ca>
2015-06-23 16:21:23 -0600

    Restored default values in sufia.rb

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -89,19 +89,8 @@ Sufia.config do |config|
   # please run rake db:seed to create the collections and restart httpd. 
   # The collection IDs will be added here
   # In production it assumes that the collections will be available in the system at the time of deposit
-
-
- 
-
-
-
-
-
-
-
-
-  config.cstr_collection_id = "4m90dw65k"
-  config.ser_collection_id = "4m90dw66v"
+  # config.cstr_collection_id = ""
+  # config.ser_collection_id = ""
   config.special_reports = {
     "cstr" => "Computing Science Technical Report",
     "ser" => "Structural Engineering Report",
@@ -154,7 +143,6 @@ Sufia.config do |config|
 
   # Specify the path to the file characterization tool:
   # config.fits_path = "fits.sh"
-   config.fits_path = "fits"
   config.fits_path = "fits"
 
   config.enable_contact_form_delivery = true
