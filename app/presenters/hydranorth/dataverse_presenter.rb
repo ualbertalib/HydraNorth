5c83ab7c1a2b3d95bb222f3ab5fe30ba2cd68e4e
Peter Binkley <peter.binkley@ualberta.ca>
2015-07-21 16:23:48 -0600

    Add identifier to Dataverse item display

--- app/presenters/hydranorth/dataverse_presenter.rb
+++ app/presenters/hydranorth/dataverse_presenter.rb
@@ -3,7 +3,7 @@ module Hydranorth
     include Hydra::Presenter
     self.model_class = ::GenericFile
     # Terms is the list of fields displayed by app/views/generic_files/_show_descriptions.html.erb
-    self.terms = [:title, :creator, :contributor, :subject, :resource_type, :language, :spatial, :temporal, :description, :date_created, :license, :rights, :is_version_of, :source, :related_url, :publisher]
+    self.terms = [:title, :creator, :contributor, :subject, :resource_type, :language, :identifier, :spatial, :temporal, :description, :date_created, :license, :rights, :is_version_of, :source, :related_url, :publisher]
 
   end
 end
