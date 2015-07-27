a52619cd95374603339c6cab429829a5506dd438
Peter Binkley <peter.binkley@ualberta.ca>
2015-07-27 17:19:53 -0600

    Fix CSTR field on metadata form

--- spec/forms/generic_file_new_form_spec.rb
+++ spec/forms/generic_file_new_form_spec.rb
@@ -15,3 +15,21 @@ describe 'unfold agreement div', :type => :feature do
   end
 end
 
+require 'spec_helper'
+describe 'request CSTR item', :type => :feature do
+  let(:user) { FactoryGirl.create :user }
+  before do
+    visit '/'
+  end
+  it "should have CSTR field" do
+    sign_in user
+    visit '/files/new'
+    choose('resource_type_Computing_Science_Technical_Report')
+    check('terms_of_service')
+    page.attach_file "files[]", ['/var/www/sites/hydranorth/spec/fixtures/world.png']
+    click_button('main_upload_start')
+    sleep(15)
+    expect(page).to have_css('input#generic_file_cstr')
+  end
+end
+
