7d85c57bb6bdd510988cb6f0aea6d97a6347fa59
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-08-12 14:48:53 -0600

    added spec tests to the migration tasks for license file and multiple file item migration

--- spec/tasks/migration_rake_spec.rb
+++ spec/tasks/migration_rake_spec.rb
@@ -6,7 +6,7 @@ describe "Migration rake tasks" do
     load File.expand_path("../../../lib/tasks/migration.rake", __FILE__)
   end
 
-  describe "migration:eraitem" do
+  describe "migration:eraitem - standard item" do
     before do
       Rake::Task.define_task(:environment)
       Rake::Task["migration:eraitem"].invoke('spec/fixtures/migration/test-metadata/standard-metadata')
@@ -17,11 +17,45 @@ describe "Migration rake tasks" do
     end
     subject { GenericFile.last }
     it "item should be migrated" do
+      expect(subject.license).to eq "Creative Commons Attribution-Non-Commercial-No Derivatives 3.0 Unported"
       expect(subject.fedora3uuid).to eq "uuid:394266f0-0e4a-42e6-a199-158165226426"
       expect(subject.content.latest_version.label).to eq "version1"
       expect(subject.fedora3foxml.latest_version.label).to eq "version1"
     end 
   end
+
+  describe "migration:eraitem - multifile item" do
+    before do
+      Rake::Task.define_task(:environment)
+      Rake::Task["migration:eraitem"].invoke('spec/fixtures/migration/test-metadata/multifile-metadata')
+    end
+    after do
+      Rake::Task["migration:eraitem"].reenable
+      GenericFile.last.delete
+    end
+    subject { GenericFile.last }
+    it "multifile item should have a zip file" do
+      expect(subject.label).to eq "uuid:846f544d-94db-41b4-9f4a-654e1457ed8c.zip"
+    end
+  end
+
+  describe "migration:eraitem - item with a license file" do
+    before do
+      Rake::Task.define_task(:environment)
+      Rake::Task["migration:eraitem"].invoke('spec/fixtures/migration/test-metadata/licensefile-metadata')
+    end
+    after do
+      Rake::Task["migration:eraitem"].reenable
+      GenericFile.last.delete
+    end
+    subject { GenericFile.last }
+    it "item should have the license file content as rights statement" do
+      expect(subject.license).to eq "I am required to use/link to a publisher's license"
+      expect(subject.rights).to eq "This material is provided under educational reproduction permissions included in Alberta Agriculture and Rural Development’s Copyright and Disclosure Statement; see terms at agriculture.alberta.ca/copyright. This Statement requires the following identification: The source of the materials is Alberta Agriculture and Rural Development, www.agriculture.alberta.ca. The use of these materials by the end user is done without any affiliation with or endorsement by the Government of Alberta. Reliance upon the end user's use of these materials is at the risk of the end user."
+    end
+  end
+
+
 
   describe "migration:era_collection_community - community" do
     before do
