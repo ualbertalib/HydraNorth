61f0d2f3e90713bdc197870622944002ded2d0c3
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-09-02 10:17:29 -0600

    update test to reflect change in #556

--- spec/features/agreement_spec.rb
+++ spec/features/agreement_spec.rb
@@ -9,7 +9,7 @@ describe "Display agreement", :type => :feature do
 
   describe "Check the text" do
     it "should have the following text" do
-      expect(page).to have_content "ERA Distribution License"
+      expect(page).to have_content "ERA Deposit and Distribution Agreement"
     end
   end
 end
