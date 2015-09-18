48503eb80d1609d3c4658e3b198717fd42a72220
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-09-18 13:50:55 -0600

    Don't need this to extend ActiveSupport::Concern or empty include block

--- app/models/concerns/hydranorth/generic_file/doi.rb
+++ app/models/concerns/hydranorth/generic_file/doi.rb
@@ -1,10 +1,7 @@
 module Hydranorth
   module GenericFile
     module DOI 
-      extend ActiveSupport::Concern
       include Linkable
-      included do
-      end
 
       def doi_url
         self.identifier.first if ((!self.identifier.first.nil?) && (linkable? self.identifier.first))
