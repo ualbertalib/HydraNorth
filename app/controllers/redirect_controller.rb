65b83ad4d4cf88fe5592a575567e37985a0cd092
pcharoen <piyapong.charoenwattana@gmail.com>
2015-09-25 10:19:16 -0600

    Updated redirect controller.

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,21 +1,37 @@
 class RedirectController < ActionController::Base
+  include Blacklight::SolrHelper
+  
+  attr_reader :id 
   
   def item
     
-    # search item by uuid
+    # search item by uuid (fedora3uuid_tesim)
+    @id = find_item_id
+    logger.debug "logger id: #{@id} "
     
     # redirect to hydranorth item
-    redirect_to '/collections/hq37vn58w'
+    redirect_to "/files/9880vq965"
     
   end
   
   def datastream
-    
+
     # construct hydranorth datastream url 
     
-    # redirect to the url
-    redirect_to '/collections/hq37vn595'
     
+    # redirect to the url
+    redirect_to "/downloads/9880vq965"
+    
+  end
+  
+  private
+  
+  def find_item_id
+    uuid = params[:uuid]
+    logger.debug "uuid: #{uuid}"
+    solr_params = "q=fedora3uuid:" + uuid
+    (@response, @member_docs) = get_search_results(solr_params)
+    @id = "9880vq965"
   end
   
 end
