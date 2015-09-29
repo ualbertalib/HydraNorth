d946d7eef327441455218da51cb808a20db66375
Peter Binkley <peter.binkley@ualberta.ca>
2015-09-29 15:08:55 -0600

    Add comment explaining communities
    Since we know that others are looking at our implementation of an IR in Sufia, we should explain our local peculiarities.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,3 +1,6 @@
+# communities are super-collections, which were needed to replicate the 
+# model of ualbertalib's legacy repository. They can contain items but
+# generally contain only collections.
 class CommunitiesController < ApplicationController
   include Hydranorth::CommunitiesControllerBehavior
 end
