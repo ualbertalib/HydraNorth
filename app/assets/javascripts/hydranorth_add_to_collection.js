a4c6e42e5c188baac1088000c0bf37b02d637d95
nnunn <natasha@nnunn.ca>
2015-10-01 14:14:56 -0600

    fixed javascript issue on add-to-communities section of upload form

--- app/assets/javascripts/hydranorth_add_to_collection.js
+++ app/assets/javascripts/hydranorth_add_to_collection.js
@@ -1,31 +1,29 @@
-  var remove_button = $("<button type=\"button\" class=\"btn btn-danger remove_collection_group\"><i class=\"icon-white glyphicon-minus\"></i><span>Remove the following community/collection group</span></button><br>");
-  $(document).on("click", ".add_collection_group", function(){
-  current = $('div[class^="community-group"]').last()
-  addNewGroup(current);
-  });
-  $(document).on("click", ".remove_collection_group", function(){
-  var current = $(this).parent()
-  removeCurrentGroup(current);
-  });
-  function addNewGroup(current){
-    current_class= current.attr('class')
-    index = current_class.split('-')[2]
-    newIndex = "community-group-" + (parseInt(index) +1).toString();
-    newGroup = current.clone().removeClass(current_class).addClass(newIndex);
-    newCommunity = newGroup.children('select#generic_file_belongsToCommunity');
-    newCommunity.removeClass(current_class).addClass(newIndex);
-    if (index == 1) {
-      newCommunity.val('').removeProp('required');
-      newCommunityLabel = newGroup.children("label[for='generic_file_belongsToCommunity']")
-      newCommunityLabel.removeClass('required');
-      newCommunityLabel.children('abbr[title="required"]').remove();
+jQuery(function($){
+    $(".add_collection_group").click(function() {
+        last_group = $(".community-group:last");
+        addNewGroup(last_group);
+      });
+      function addNewGroup(last_group){
+        current_class = last_group.attr('id');
+        last_id = last_group.attr('id');
+        index = last_id.split('-')[2]
+        new_id = "community-group-" + (parseInt(index) +1).toString();
+        newGroup = last_group.clone().removeAttr("id").attr("id", new_id);
+        newCommunity = newGroup.children('select#generic_file_belongsToCommunity');
+        newCommunity.removeClass(current_class).addClass(new_id);
+        if (index == 1){
+          newGroup.append( $("<button type='button' class='btn btn-danger remove_collection_group'><i class='icon-white glyphicon-minus'></i><span> Remove this Community/Collection group</span></button>") )
+          newCommunity.val('').removeProp('required');
+          newCommunityLabel = newGroup.children("label[for='generic_file_belongsToCommunity']")
+          newCommunityLabel.removeClass('required');
+          newCommunityLabel.children('abbr[title="required"]').remove();
+        }
+        newCollection = newGroup.children('select#generic_file_hasCollectionId');
+        newCollection.removeClass(current_class).addClass(new_id);
+        newCommunity.first().focus();
+        newGroup.appendTo("#additional");
     }
-    newCollection = newGroup.children('select#generic_file_hasCollectionId');
-    newCollection.removeClass(current_class).addClass(newIndex);
-    newCommunity.first().focus();
-    newGroup.prepend(remove_button);
-    newGroup.insertAfter(current);
-  }
-  function removeCurrentGroup(current){
-    current.remove()
-  }
+    $("body").on('click',".remove_collection_group", function() {
+        $(this).closest(".community-group").remove();
+    });
+});
