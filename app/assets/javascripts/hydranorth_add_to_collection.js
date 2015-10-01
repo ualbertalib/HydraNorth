b0111be99f7fb3a66ec5087a75bf1aae451f30bb
nnunn <natasha@nnunn.ca>
2015-10-01 15:15:23 -0600

    fixed javascript issue on add-to-communities section of upload form

--- app/assets/javascripts/hydranorth_add_to_collection.js
+++ app/assets/javascripts/hydranorth_add_to_collection.js
@@ -4,13 +4,12 @@ jQuery(function($){
         addNewGroup(last_group);
       });
       function addNewGroup(last_group){
-        current_class = last_group.attr('id');
         last_id = last_group.attr('id');
         index = last_id.split('-')[2]
         new_id = "community-group-" + (parseInt(index) +1).toString();
         newGroup = last_group.clone().removeAttr("id").attr("id", new_id);
         newCommunity = newGroup.children('select#generic_file_belongsToCommunity');
-        newCommunity.removeClass(current_class).addClass(new_id);
+        newCommunity.removeClass(last_id).addClass(new_id);
         if (index == 1){
           newGroup.append( $("<button type='button' class='btn btn-danger remove_collection_group'><i class='icon-white glyphicon-minus'></i><span> Remove this Community/Collection group</span></button>") )
           newCommunity.val('').removeProp('required');
@@ -19,7 +18,7 @@ jQuery(function($){
           newCommunityLabel.children('abbr[title="required"]').remove();
         }
         newCollection = newGroup.children('select#generic_file_hasCollectionId');
-        newCollection.removeClass(current_class).addClass(new_id);
+        newCollection.removeClass(last_id).addClass(new_id);
         newCommunity.first().focus();
         newGroup.appendTo("#additional");
     }
