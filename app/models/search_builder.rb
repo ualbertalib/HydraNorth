7de9e84b1f3d995ddcf99a9c0bf055cdce063c3b
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-10-15 16:26:01 -0600

    quick fix #724

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,4 +2,9 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include Hydra::AccessControlsEnforcement
   include Sufia::SearchBuilder
+
+   def include_collection_ids(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << "collection_tesim:#{collection.id} OR hasCollectionId_ssim:#{collection.id}"
+  end
 end
