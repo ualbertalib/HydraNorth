532516b3a9104d57e4f5a29b47b5aeb2c79b58c2
Peter Binkley <peter.binkley@ualberta.ca>
2015-10-15 17:44:19 -0600

    Retrieve collections using belongsToCommunity_tesim
    This will retrieve grandchild collections if we allow nesting.
    There is no nesting in old ERA, so this is not a problem with
    migrated data.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,6 +5,6 @@ class SearchBuilder < Blacklight::SearchBuilder
 
    def include_collection_ids(solr_parameters)
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "collection_tesim:#{collection.id} OR hasCollectionId_ssim:#{collection.id}"
+    solr_parameters[:fq] << "(active_fedora_model_ssi:Collection AND belongsToCommunity_tesim:#{collection.id}) OR hasCollectionId_ssim:#{collection.id}"
   end
 end
