4d869a6f352d67dabc989a9ca40cd5b78cc11ec4
criedlberger <riedlber@ualberta.ca>
2015-11-25 12:15:22 -0700

    Allow old tests to pass

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -10,32 +10,50 @@ class CatalogController < ApplicationController
   before_filter :enforce_show_permissions, only: :show
   before_action :set_solr_search_fields
   # This applies appropriate access controls to all solr queries
-  #  CatalogController.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
+  CatalogController.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
 
   skip_before_filter :default_html_head
 
   def self.uploaded_field
-    Solrizer.solr_name('date_uploaded', :stored_sortable, type: :date)
+    solr_name('date_uploaded', :stored_sortable, type: :date)
   end
 
   def self.modified_field
-    Solrizer.solr_name('date_modified', :stored_sortable, type: :date)
+    solr_name('date_modified', :stored_sortable, type: :date)
   end
 
   def self.date_created_field
-    value = Solrizer.solr_name('date_created', :stored_sortable, type: :string)
+    value = solr_name('date_created', :stored_sortable, type: :string)
     logger.debug "SOLR_SORT_DATE#{value}" 
     return value
   end
 
   def set_solr_search_fields
+
     blacklight_config.configure do |config|
 
-      ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
+      if !current_user.nil?
+        if current_user.admin?
+          self.search_params_logic -= [:add_access_controls_to_solr_params]
+          self.search_params_logic += [:add_advanced_parse_q_to_solr]
+        else
+          self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
+        end
+      else
+        self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
+      end
+
       config.default_solr_params = {
         qt: "search",
         rows: 10
-      }      
+      }
+
+      if !current_user.nil?
+        if current_user.admin?
+          config.add_facet_field Solrizer.solr_name("depositor", :symbol), label: "Depositor", limit: 3
+          config.add_facet_field Solrizer.solr_name("read_access_group", :symbol), label: "Status", missing: true, limit: 3
+        end
+      end
 
       config.add_search_field('all_fields', label: 'Keyword', include_in_advanced_search: true) do |field|
         all_names = config.show_fields.values.map{|val| val.field}.join(" ")
@@ -121,7 +139,7 @@ class CatalogController < ApplicationController
       config.add_search_field('language') do |field|
         field.solr_parameters = {
           :"spellcheck.dictionary" => "language"
-        } 
+        }
         solr_name = Solrizer.solr_name("language", :stored_searchable)
         field.solr_local_parameters = {
           qf: solr_name,
@@ -143,102 +161,88 @@ class CatalogController < ApplicationController
     end
   end
 
-  def index
-    if !current_user.nil?
-      if current_user.admin?
-        self.search_params_logic += [:add_advanced_parse_q_to_solr]
-      else
-        self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
-      end
-    else
-      self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
-    end
+
+  configure_blacklight do |config|
+
+    ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
+    config.default_solr_params = {
+      qt: "search",
+      rows: 10
+    }
+
+    # Specify which field to use in the tag cloud on the homepage.
+    # To disable the tag cloud, comment out this line.
+    # config.tag_cloud_field_name = Solrizer.solr_name("tag", :facetable)
+
+    # solr field configuration for document/show views
+    config.index.title_field = solr_name("title", :stored_searchable)
+    config.index.display_type_field = solr_name("has_model", :symbol)
+    config.index.thumbnail_method = :sufia_thumbnail_tag
+
+    # solr fields that will be treated as facets by the blacklight application
+    #   The ordering of the field names is the order of the display
+    config.add_facet_field solr_name("resource_type", :facetable), label: "Item Type", limit: 3
+    config.add_facet_field solr_name("creator", :facetable), label: "Author", limit: 3
+    config.add_facet_field solr_name("subject", :facetable), label: "Subject", limit: 3
+    config.add_facet_field solr_name("language", :facetable), label: "Language", limit: 3
+    config.add_facet_field solr_name("hasCollection", :symbol), label: "Collection", limit: 3 
+    config.add_facet_field solr_name("year_created", :facetable), label: "Year", limit: 3 
+    # publisher: has "show: false", but is needed to provide field label in "You searched for" box
+    config.add_facet_field solr_name("publisher", :facetable), label: "Publisher", show: false
+
+    # Have BL send all facet field names to Solr, which has been the default
+    # previously. Simply remove these lines if you'd rather use Solr request
+    # handler defaults, or have no facets.
+    config.add_facet_fields_to_solr_request!
+
+    # solr fields to be displayed in the index (search results) view
+    #   The ordering of the field names is the order of the display
+    config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name'
+    config.add_index_field solr_name("description", :stored_searchable), label: "Description", itemprop: 'description'
+    config.add_index_field solr_name("subject", :stored_searchable), label: "Subject(s)", itemprop: 'about'
+    config.add_index_field solr_name("creator", :stored_searchable), label: "Creator(s)", itemprop: 'creator'
+    config.add_index_field solr_name("contributor", :stored_searchable), label: "Contributor", itemprop: 'contributor'
+    config.add_index_field solr_name("spatial", :stored_searchable), label: "Spatial", itemprop: 'contentLocation'
+    config.add_index_field solr_name("temporal", :stored_searchable), label: "Temporal", itemprop: 'contentTemporal'
+    config.add_index_field solr_name("language", :stored_searchable), label: "Language", itemprop: 'inLanguage'
+    config.add_index_field solr_name("date_uploaded", :stored_searchable), label: "Date Uploaded", itemprop: 'datePublished'
+    config.add_index_field solr_name("date_modified", :stored_searchable), label: "Date Modified", itemprop: 'dateModified'
+    config.add_index_field solr_name("date_created", :stored_searchable), label: "Date Created", itemprop: 'dateCreated'
+    config.add_index_field solr_name("license", :stored_searchable), label: "License"
+    config.add_index_field solr_name("resource_type", :stored_searchable), label: "Resource Type"
+    config.add_index_field solr_name("format", :stored_searchable), label: "File Format"
+    config.add_index_field solr_name("identifier", :stored_searchable), label: "Identifier"
 
 
-    blacklight_config.configure do |config|
+    # solr fields to be displayed in the show (single result) view
+    #   The ordering of the field names is the order of the display
+    config.add_show_field solr_name("title", :stored_searchable), label: "Title"
+    config.add_show_field solr_name("description", :stored_searchable), label: "Description"
+    config.add_show_field solr_name("subject", :stored_searchable), label: "Subject"
+    config.add_show_field solr_name("creator", :stored_searchable), label: "Creator"
+    config.add_show_field solr_name("contributor", :stored_searchable), label: "Contributor"
+    config.add_show_field solr_name("spatial", :stored_searchable), label: "Location"
+    config.add_show_field solr_name("temporal", :stored_searchable), label: "Time"
+    config.add_show_field solr_name("language", :stored_searchable), label: "Language"
+    config.add_show_field solr_name("date_uploaded", :stored_searchable), label: "Date Uploaded"
+    config.add_show_field solr_name("date_modified", :stored_searchable), label: "Date Modified"
+    config.add_show_field solr_name("date_created", :stored_searchable), label: "Date Created"
+    config.add_show_field solr_name("license", :stored_searchable), label: "License"
+    config.add_show_field solr_name("resource_type", :stored_searchable), label: "Resource Type"
+    config.add_show_field solr_name("format", :stored_searchable), label: "File Format"
+    config.add_show_field solr_name("trid", :stored_searchable), label: "CS Technical Report ID"
+    config.add_show_field solr_name("ser", :stored_searchable), label: "Structural Engineering Report ID"
+    config.add_show_field solr_name("publisher", :stored_searchable), label: "Publisher"
+    config.add_show_field solr_name("fedora3uuid", :stored_searchable), label: "UUID"
 
-      # Specify which field to use in the tag cloud on the homepage.
-      # To disable the tag cloud, comment out this line.
-      # config.tag_cloud_field_name = Solrizer.solr_name("tag", :facetable)
+    config.add_sort_field "score desc, #{uploaded_field} desc", label: "Relevance \u25BC"
+    config.add_sort_field "#{date_created_field} desc", label: "Date (newest first)"
+    config.add_sort_field "#{date_created_field} asc", label: "Date (oldest first)"
+    config.add_sort_field "#{uploaded_field} desc", label: "New items"
 
-      # solr field configuration for document/show views
-      config.index.title_field = Solrizer.solr_name("title", :stored_searchable)
-      config.index.display_type_field = Solrizer.solr_name("has_model", :symbol)
-      config.index.thumbnail_method = :sufia_thumbnail_tag
-
-      # solr fields that will be treated as facets by the blacklight application
-      #   The ordering of the field names is the order of the display
-      config.add_facet_field Solrizer.solr_name("resource_type", :facetable), label: "Item Type", limit: 3
-      config.add_facet_field Solrizer.solr_name("creator", :facetable), label: "Author", limit: 3
-      config.add_facet_field Solrizer.solr_name("subject", :facetable), label: "Subject", limit: 3
-      config.add_facet_field Solrizer.solr_name("language", :facetable), label: "Language", limit: 3
-      config.add_facet_field Solrizer.solr_name("hasCollection", :symbol), label: "Collection", limit: 3 
-      config.add_facet_field Solrizer.solr_name("year_created", :facetable), label: "Year", limit: 3 
-      if !current_user.nil?
-        if current_user.admin?
-          config.add_facet_field Solrizer.solr_name("depositor", :symbol), label: "Depositor", limit: 3
-          config.add_facet_field Solrizer.solr_name("read_access_group", :symbol), label: "Status", missing: true, limit: 3
-        end
-      end
-
-      # publisher: has "show: false", but is needed to provide field label in "You searched for" box
-      config.add_facet_field Solrizer.solr_name("publisher", :facetable), label: "Publisher", show: false
-
-      # Have BL send all facet field names to Solr, which has been the default
-      # previously. Simply remove these lines if you'd rather use Solr request
-      # handler defaults, or have no facets.
-      config.add_facet_fields_to_solr_request!
-
-      # solr fields to be displayed in the index (search results) view
-      #   The ordering of the field names is the order of the display
-      config.add_index_field Solrizer.solr_name("title", :stored_searchable), label: "Title", itemprop: 'name'
-      config.add_index_field Solrizer.solr_name("description", :stored_searchable), label: "Description", itemprop: 'description'
-      config.add_index_field Solrizer.solr_name("subject", :stored_searchable), label: "Subject(s)", itemprop: 'about'
-      config.add_index_field Solrizer.solr_name("creator", :stored_searchable), label: "Creator(s)", itemprop: 'creator'
-      config.add_index_field Solrizer.solr_name("contributor", :stored_searchable), label: "Contributor", itemprop: 'contributor'
-      config.add_index_field Solrizer.solr_name("spatial", :stored_searchable), label: "Spatial", itemprop: 'contentLocation'
-      config.add_index_field Solrizer.solr_name("temporal", :stored_searchable), label: "Temporal", itemprop: 'contentTemporal'
-      config.add_index_field Solrizer.solr_name("language", :stored_searchable), label: "Language", itemprop: 'inLanguage'
-      config.add_index_field Solrizer.solr_name("date_uploaded", :stored_searchable), label: "Date Uploaded", itemprop: 'datePublished'
-      config.add_index_field Solrizer.solr_name("date_modified", :stored_searchable), label: "Date Modified", itemprop: 'dateModified'
-      config.add_index_field Solrizer.solr_name("date_created", :stored_searchable), label: "Date Created", itemprop: 'dateCreated'
-      config.add_index_field Solrizer.solr_name("license", :stored_searchable), label: "License"
-      config.add_index_field Solrizer.solr_name("resource_type", :stored_searchable), label: "Resource Type"
-      config.add_index_field Solrizer.solr_name("format", :stored_searchable), label: "File Format"
-      config.add_index_field Solrizer.solr_name("identifier", :stored_searchable), label: "Identifier"
-
-
-      # solr fields to be displayed in the show (single result) view
-      #   The ordering of the field names is the order of the display
-      config.add_show_field Solrizer.solr_name("title", :stored_searchable), label: "Title"
-      config.add_show_field Solrizer.solr_name("description", :stored_searchable), label: "Description"
-      config.add_show_field Solrizer.solr_name("subject", :stored_searchable), label: "Subject"
-      config.add_show_field Solrizer.solr_name("creator", :stored_searchable), label: "Creator"
-      config.add_show_field Solrizer.solr_name("contributor", :stored_searchable), label: "Contributor"
-      config.add_show_field Solrizer.solr_name("spatial", :stored_searchable), label: "Location"
-      config.add_show_field Solrizer.solr_name("temporal", :stored_searchable), label: "Time"
-      config.add_show_field Solrizer.solr_name("language", :stored_searchable), label: "Language"
-      config.add_show_field Solrizer.solr_name("date_uploaded", :stored_searchable), label: "Date Uploaded"
-      config.add_show_field Solrizer.solr_name("date_modified", :stored_searchable), label: "Date Modified"
-      config.add_show_field Solrizer.solr_name("date_created", :stored_searchable), label: "Date Created"
-      config.add_show_field Solrizer.solr_name("license", :stored_searchable), label: "License"
-      config.add_show_field Solrizer.solr_name("resource_type", :stored_searchable), label: "Resource Type"
-      config.add_show_field Solrizer.solr_name("format", :stored_searchable), label: "File Format"
-      config.add_show_field Solrizer.solr_name("trid", :stored_searchable), label: "CS Technical Report ID"
-      config.add_show_field Solrizer.solr_name("ser", :stored_searchable), label: "Structural Engineering Report ID"
-      config.add_show_field Solrizer.solr_name("publisher", :stored_searchable), label: "Publisher"
-      config.add_show_field Solrizer.solr_name("fedora3uuid", :stored_searchable), label: "UUID"
-
-      config.add_sort_field "score desc, #{Solrizer.solr_name('date_uploaded', :stored_sortable, type: :date)} desc", label: "Relevance \u25BC"
-      config.add_sort_field "#{Solrizer.solr_name('date_created', :stored_sortable, type: :string)} desc", label: "Date (newest first)"
-      config.add_sort_field "#{Solrizer.solr_name('date_created', :stored_sortable, type: :string)} asc", label: "Date (oldest first)"
-      config.add_sort_field "#{Solrizer.solr_name('date_uploaded', :stored_sortable, type: :date)} desc", label: "New items"
-
-      # If there are more than this many search results, no spelling ("did you
-      # mean") suggestion is offered.
-      config.spell_max = 5
-    end
-    super
+    # If there are more than this many search results, no spelling ("did you
+    # mean") suggestion is offered.
+    config.spell_max = 5
   end
 
 end
