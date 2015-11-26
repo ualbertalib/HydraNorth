2ed84958f39abcdb1052e95f9de52442b441a5bc
criedlberger <riedlber@ualberta.ca>
2015-11-26 10:37:55 -0700

    860 sort mod date

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -8,6 +8,7 @@ class CatalogController < ApplicationController
 
   # These before_filters apply the hydra access controls
   before_filter :enforce_show_permissions, only: :show
+  before_action :set_solr_search_fields
   # This applies appropriate access controls to all solr queries
   CatalogController.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
 
@@ -26,6 +27,140 @@ class CatalogController < ApplicationController
     logger.debug "SOLR_SORT_DATE#{value}" 
     return value
   end
+
+  def set_solr_search_fields
+
+    blacklight_config.configure do |config|
+
+      if !current_user.nil?
+        if current_user.admin?
+          self.search_params_logic -= [:add_access_controls_to_solr_params]
+          self.search_params_logic += [:add_advanced_parse_q_to_solr]
+        else
+          self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
+        end
+      else
+        self.search_params_logic += [:add_access_controls_to_solr_params, :add_advanced_parse_q_to_solr]
+      end
+
+      config.default_solr_params = {
+        qt: "search",
+        rows: 10
+      }
+
+      if !current_user.nil?
+        if current_user.admin?
+          config.add_facet_field Solrizer.solr_name("depositor", :symbol), label: "Depositor", limit: 3
+          config.add_facet_field Solrizer.solr_name("read_access_group", :symbol), label: "Status", missing: true, limit: 3
+        end
+      end
+
+      config.add_search_field('all_fields', label: 'Keyword', include_in_advanced_search: true) do |field|
+        all_names = config.show_fields.values.map{|val| val.field}.join(" ")
+        title_name = Solrizer.solr_name("title", :stored_searchable)
+        field.solr_parameters = {
+         qf: "#{all_names} noid_tsi file_format_tesim all_text_timv",
+          pf: "#{title_name}"
+        }
+      end
+
+      config.add_search_field('author', label: 'Author', include_in_advanced_search: true) do |field|
+        field.solr_parameters = { :"spellcheck.dictionary" => "contributor", :"spellcheck.dictionary" => "creator" }
+        field_included = [Solrizer.solr_name("contributor", :stored_searchable), Solrizer.solr_name("creator", :stored_searchable)].join(" ")
+        field.solr_parameters = {
+          qf: field_included,
+          pf: field_included
+        }
+      end
+
+      if !current_user.nil?
+        if current_user.admin?
+          config.add_search_field('User') do |field|
+            field.solr_parameters = {
+              :"spellcheck.dictionary" => "edit_access_person"
+            }
+            solr_name = Solrizer.solr_name("edit_access_person", :symbol)
+            field.solr_local_parameters = {
+              qf: solr_name,
+              pf: solr_name
+            }
+          end
+        end
+      end
+
+      config.add_search_field('title') do |field|
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "title"
+        }
+        solr_name = Solrizer.solr_name("title", :stored_searchable)
+        field.solr_local_parameters = {
+          qf: solr_name,
+          pf: solr_name
+        }
+      end
+
+      config.add_search_field('description') do |field|
+        field.label = "Abstract or Summary"
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "description"
+        }
+        solr_name = Solrizer.solr_name("description", :stored_searchable)
+        field.solr_local_parameters = {
+          qf: solr_name,
+          pf: solr_name
+        }
+      end
+
+      config.add_search_field('date_created') do |field|
+        field.label = "Date"
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "date_created"
+        }
+        solr_name = Solrizer.solr_name("created", :stored_searchable)
+        field.solr_local_parameters = {
+          qf: solr_name,
+          pf: solr_name
+        }
+      end
+
+      config.add_search_field('allsubject', label: 'Subject', include_in_advanced_search: true) do |field|
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "subject",
+          :"spellcheck.dictionary" => "temporal",
+          :"spellcheck.dictionary" => "spatial"
+        }
+        field_included = [Solrizer.solr_name("subject", :stored_searchable), Solrizer.solr_name("spatial", :stored_searchable), Solrizer.solr_name("temporal", :stored_searchable)].join(" ")
+        field.solr_local_parameters = {
+          qf: field_included,
+          pf: field_included
+        }
+      end
+
+      config.add_search_field('language') do |field|
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "language"
+        }
+        solr_name = Solrizer.solr_name("language", :stored_searchable)
+        field.solr_local_parameters = {
+          qf: solr_name,
+          pf: solr_name
+        }
+      end
+
+      config.add_search_field('resource_type') do |field|
+        field.label = "Item Type"
+        field.solr_parameters = {
+          :"spellcheck.dictionary" => "resource_type"
+        }
+        solr_name = Solrizer.solr_name("resource_type", :stored_searchable)
+        field.solr_local_parameters = {
+          qf: solr_name,
+          pf: solr_name
+        }
+      end
+    end
+  end
+
 
   configure_blacklight do |config|
 
@@ -100,151 +235,12 @@ class CatalogController < ApplicationController
     config.add_show_field solr_name("publisher", :stored_searchable), label: "Publisher"
     config.add_show_field solr_name("fedora3uuid", :stored_searchable), label: "UUID"
 
-    # "fielded" search configuration. Used by pulldown among other places.
-    # For supported keys in hash, see rdoc for Blacklight::SearchFields
-    #
-    # Search fields will inherit the :qt solr request handler from
-    # config[:default_solr_parameters], OR can specify a different one
-    # with a :qt key/value. Below examples inherit, except for subject
-    # that specifies the same :qt as default for our own internal
-    # testing purposes.
-    #
-    # The :key is what will be used to identify this BL search field internally,
-    # as well as in URLs -- so changing it after deployment may break bookmarked
-    # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different.
-    #
-    # This one uses all the defaults set by the solr request handler. Which
-    # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise.
-    config.add_search_field('all_fields', label: 'Keyword', include_in_advanced_search: true) do |field|
-      all_names = config.show_fields.values.map{|val| val.field}.join(" ")
-      title_name = solr_name("title", :stored_searchable)
-      field.solr_parameters = {
-        qf: "#{all_names} noid_tsi file_format_tesim all_text_timv",
-        pf: "#{title_name}"
-      }
-    end
-
-    # Now we see how to over-ride Solr request handler defaults, in this
-    # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields.
-    # creator, title, description, temporal, spatial, date_created,
-    # subject, language, resource_type, format, identifier, spatial
-
-    config.add_search_field('author', label: 'Author', include_in_advanced_search: true) do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :"spellcheck.dictionary" => "contributor", :"spellcheck.dictionary" => "creator" }
-      
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-
-      field_included = [solr_name("contributor", :stored_searchable), solr_name("creator", :stored_searchable)].join(" ")
-      field.solr_parameters = {
-        qf: field_included,
-        pf: field_included
-      }
-    end
-    
-    config.add_search_field('title') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "title"
-      }
-      solr_name = solr_name("title", :stored_searchable)
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
-
-    config.add_search_field('description') do |field|
-      field.label = "Abstract or Summary"
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "description"
-      }
-      solr_name = solr_name("description", :stored_searchable)
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
-
-    config.add_search_field('date_created') do |field|
-      field.label = "Date"
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "date_created"
-      }
-      solr_name = solr_name("created", :stored_searchable)
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
-
-
-    config.add_search_field('allsubject', label: 'Subject', include_in_advanced_search: true) do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "subject",
-        :"spellcheck.dictionary" => "temporal",
-        :"spellcheck.dictionary" => "spatial"
-      }
-      field_included = [solr_name("subject", :stored_searchable), solr_name("spatial", :stored_searchable), solr_name("temporal", :stored_searchable)].join(" ")
-      field.solr_local_parameters = {
-        qf: field_included,
-        pf: field_included 
-      }
-    end
-    
-    config.add_search_field('language') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "language"
-      }
-      solr_name = solr_name("language", :stored_searchable)
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
-
-    config.add_search_field('resource_type') do |field|
-      field.label = "Item Type"
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "resource_type"
-      }
-      solr_name = solr_name("resource_type", :stored_searchable)
-      field.solr_local_parameters = {
-        qf: solr_name,
-        pf: solr_name
-      }
-    end
-    # these two are something we need to enable for admin
-    #config.add_search_field('depositor') do |field|
-    #  solr_name = solr_name("depositor", :stored_searchable)
-    #  field.solr_local_parameters = {
-    #    qf: solr_name,
-    #    pf: solr_name
-    #  }
-    #end
-
-    #config.add_search_field('license') do |field|
-    #  solr_name = solr_name("license", :stored_searchable)
-    #  field.solr_local_parameters = {
-    #    qf: solr_name,
-    #    pf: solr_name
-    #  }
-    #end
-
-    # "sort results by" select (pulldown)
-    # label in pulldown is followed by the name of the SOLR field to sort by and
-    # whether the sort is ascending or descending (it must be asc or desc
-    # except in the relevancy case).
-    # label is key, solr field is value
     config.add_sort_field "score desc, #{uploaded_field} desc", label: "Relevance \u25BC"
     config.add_sort_field "#{date_created_field} desc", label: "Date (newest first)"
     config.add_sort_field "#{date_created_field} asc", label: "Date (oldest first)"
     config.add_sort_field "#{uploaded_field} desc", label: "New items"
+    config.add_sort_field "#{modified_field} desc", label: "Date modified (newest first)"
+    config.add_sort_field "#{modified_field} asc", label: "Date modified (oldest first)"
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
