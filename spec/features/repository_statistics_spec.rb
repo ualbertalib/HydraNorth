054c9fcd79590e38b997b2e8ac82bd363796eafc
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2015-12-22 14:08:42 -0700

    mock solr response so there is something to chartify on #686

--- spec/features/repository_statistics_spec.rb
+++ spec/features/repository_statistics_spec.rb
@@ -4,6 +4,7 @@ describe RepositoryStatisticsController, type: :feature do
 
   context '#facet_stats' do
     before do
+      allow(ActiveFedora::SolrService.instance.conn).to receive(:get) { JSON.parse("{\"responseHeader\":{\"status\":0,\"QTime\":7,\"params\":{\"q\":\"active_fedora_model_ssi:\\\"GenericFile\\\"\",\"facet.field\":\"resource_type_sim\",\"rows\":\"0\",\"wt\":\"json\",\"facet\":\"true\"}},\"response\":{\"numFound\":38184,\"start\":0,\"maxScore\":2.328004,\"docs\":[]},\"facet_counts\":{\"facet_queries\":{},\"facet_fields\":{\"resource_type_sim\":[\"Thesis\",18261,\"Report\",9452,\"Image\",5521,\"Journal Article (Published)\",2171,\"Research Material\",609,\"Conference/workshop Presentation\",381,\"Dataset\",316,\"Computing Science Technical Report\",255,\"Review\",215,\"Structural Engineering Report\",187]},\"facet_dates\":{},\"facet_ranges\":{},\"facet_intervals\":{}},\"spellcheck\":{\"suggestions\":[\"correctlySpelled\",true]}}\n", :symbolize_names => true) }
       visit '/stats.json'
     end
 
