927833b0423ff4e57fe2f852b6a55251847505ea
Matt Barnett <matt@sixtyodd.com>
2016-01-14 15:05:28 -0700

    Correct description form param to pass array value
    aka Paramater single-or-multi-valuedness is critically important to form validation and security permissions. Closes #922.

--- app/forms/hydranorth/forms/generic_file_edit_form.rb
+++ app/forms/hydranorth/forms/generic_file_edit_form.rb
@@ -5,12 +5,12 @@ module Hydranorth
       include HydraEditor::Form
       include HydraEditor::Form::Permissions
       self.required_fields = [:title, :resource_type, :language, :creator, :subject, :license, :belongsToCommunity ]
-      
+
 
       # This is required so that fields_for will draw a nested form.
       # See ActionView::Helpers#nested_attributes_association?
       #   https://github.com/rails/rails/blob/a04c0619617118433db6e01b67d5d082eaaa0189/actionview/lib/action_view/helpers/form_helper.rb#L1890
-      def permissions_attributes= attributes
+      def permissions_attributes=(attributes)
         model.permissions_attributes= attributes
       end
 
