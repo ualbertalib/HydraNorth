45d7a2700a0fea1c3c857a2ffbaa18c89e46c225
criedlberger <riedlber@ualberta.ca>
2016-01-22 10:46:20 -0700

    930 ark rake

---
+++ lib/tasks/ezid/ezid_logger.rb
@@ -0,0 +1,33 @@
+#Custom Migration Logger
+require 'singleton'
+
+class EzidLogger < Logger
+  include Singleton
+
+    # Optional, but good for prefixing timestamps automatically
+  class Formatter
+    def call(severity, time, progname, msg)
+      formatted_severity = sprintf("%-5s",severity.to_s)
+      formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
+      "[#{formatted_severity} #{formatted_time}] #{msg.strip}\n"
+    end
+  end
+
+  def initialize
+    if Rails.env.test?
+      super(Rails.root.join('log/ezid-spec.log'))
+    else
+      super(Rails.root.join('log/ezid.log'))
+    end
+    self.formatter = Formatter.new
+    self
+  end
+
+  def self.error(msg); instance.error(msg) end
+  def self.debug(msg); instance.debug(msg) end
+  def self.fatal(msg); instance.fatal(msg) end
+  def self.info(msg); instance.info(msg) end
+  def self.warn(msg); instance.warn(msg) end
+  def self.add(msg); instance.add(msg) end
+  def self.log(msg); instance.log(msg) end
+end
