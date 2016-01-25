072c398f5ddd57bb605ea6ca311e84752e6e4269
criedlberger <riedlber@ualberta.ca>
2016-01-25 11:31:19 -0700

    927 ark route

--- spec/controllers/redirect_controller_spec.rb
+++ spec/controllers/redirect_controller_spec.rb
@@ -5,12 +5,19 @@ require 'fileutils'
 describe RedirectController, type: :controller do
   routes { Rails.application.class.routes }
 
+  let!(:file) do
+    GenericFile.new do |gf|
+      gf.apply_depositor_metadata('dittest@ualberta.ca')
+      gf.save!
+    end
+  end
+
   before :all do
     load File.expand_path("../../../lib/tasks/migration.rake", __FILE__)
   end
 
   describe "#item" do
-    before :all do
+    before do
       Collection.delete_all
       @community = Collection.new(title: 'test community').tap do |c|
         c.apply_depositor_metadata('dittest@ualberta.ca')
@@ -28,10 +35,11 @@ describe RedirectController, type: :controller do
       Rake::Task.define_task(:environment)
       Rake::Task["migration:eraitem"].invoke('spec/fixtures/migration/test-metadata/standard-metadata')
     end
-    after :all do
+    after do
       Rake::Task["migration:eraitem"].reenable
       GenericFile.last.delete
     end
+
     subject { GenericFile.last }
     it "Item should be migrated" do
       expect(subject.fedora3uuid).to eq "uuid:394266f0-0e4a-42e6-a199-158165226426"
@@ -39,6 +47,19 @@ describe RedirectController, type: :controller do
     it "redirects to item page" do
       get :item, uuid: "uuid:394266f0-0e4a-42e6-a199-158165226426"
       result = ActiveFedora::SolrService.instance.conn.get "select", params: {q:["fedora3uuid_tesim:uuid:394266f0-0e4a-42e6-a199-158165226426"]}
+      doc = result["response"]["docs"].first
+      id = doc["id"]
+      expect(response).to redirect_to "http://test.host/files/#{id}"
+    end
+    it "ark redirects to item page" do
+      identifier = Ezid::Identifier.create(id: "ark:/99999/fk4#{file.id}")
+      GenericFile.find(file.id) do |gf|
+        gf.ark_id = identifier.id
+        gf.save!
+      end
+
+      get :ark, arkid: "ark:/99999/fk4#{file.id}"
+      result = ActiveFedora::SolrService.instance.conn.get "select", params: {q:["ark_id_tesim:ark:/99999/fk4#{file.id}"]}
       doc = result["response"]["docs"].first
       id = doc["id"]
       expect(response).to redirect_to "http://test.host/files/#{id}"
