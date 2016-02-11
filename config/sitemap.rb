1531b22112ee4a064df09e82d6ce15e4f75a49b4
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-02-11 10:51:01 -0700

    document usage & refactor to use solr only for performance #850

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,10 +5,17 @@ Sitemap::Generator.instance.load(host: 'era.library.ualberta.ca') do
     literal Sufia::Engine.routes.url_helpers.profile_path(user.to_param), priority: 0.8, change_frequency: 'daily'
   end
   read_group = Solrizer.solr_name('read_access_group', :symbol)
-  GenericFile.where(read_group => 'public').each do |f|
-    literal Sufia::Engine.routes.url_helpers.generic_file_path(f), priority: 1, change_frequency: 'weekly'
+  solr_rsp = ActiveFedora::SolrService.instance.conn.get "select", :params => {:q => Solrizer.solr_name('read_access_group', :symbol)+':public', :fl => "id,#{Solrizer.solr_name('active_fedora_model', :stored_sortable)}"} 
+  numFound = solr_rsp['response']['numFound']
+  if 10 < numFound
+    solr_rsp = ActiveFedora::SolrService.instance.conn.get "select", :params => {:q => Solrizer.solr_name('read_access_group', :symbol)+':public', :fl => "id,#{Solrizer.solr_name('active_fedora_model', :stored_sortable)}", :rows => numFound } 
   end
-  Collection.where(read_group => 'public').each do |c|
-    literal Hydra::Collections::Engine.routes.url_helpers.collection_path(c), priority: 1, change_frequency: 'weekly'
+  solr_rsp['response']['docs'].each do |o|
+    case o[Solrizer.solr_name('active_fedora_model', :stored_sortable)]
+    when 'GenericFile'
+      literal Sufia::Engine.routes.url_helpers.generic_file_path(o['id']), priority: 1, change_frequency: 'weekly'
+    when 'Collection'
+      literal Hydra::Collections::Engine.routes.url_helpers.collection_path(o['id']), priority: 1, change_frequency: 'weekly'
+    end
   end
 end
