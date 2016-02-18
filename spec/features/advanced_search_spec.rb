962f4b9ab7377217bb1aa178fdd5053dffb36fc7
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-02-18 15:34:27 -0700

    advanced search test shouldn't depend on migration #947

--- spec/features/advanced_search_spec.rb
+++ spec/features/advanced_search_spec.rb
@@ -4,42 +4,32 @@ require 'fileutils'
 
 describe "Advanced search", :type => :feature do
   before do
-    load File.expand_path("../../../lib/tasks/migration.rake", __FILE__)
     visit "/advanced"
   end
 
   describe "Check keyword search" do
-    before do
-      Collection.delete_all
-      @community = Collection.new(title: 'test community').tap do |c|
-        c.apply_depositor_metadata('dittest@ualberta.ca')
-        c.is_community = true
-        c.is_official = true
-        c.fedora3uuid = 'uuid:d04b3b74-211d-4939-9660-c390958fa2ee'
-        c.save
+    let!(:item1) do
+      GenericFile.new.tap do |r|
+        r.title = ['Bison sculpture at the entrance to the USGS Ice Core Lab']
+        r.apply_depositor_metadata 'jilluser@example.com'
+        r.fedora3uuid = 'uuid:394266f0-0e4a-42e6-a199-158165226426' 
+        r.read_groups = ['public']
+        r.save!
       end
-      @collection = Collection.new(title: 'test collection').tap do |c|
-        c.apply_depositor_metadata('dittest@ualberta.ca')
-        c.is_official = true
-        c.fedora3uuid = 'uuid:3f5739f8-4344-4ce5-9f85-9bda224b41d7'
-        c.save
-      end
-      Rake::Task.define_task(:environment)
-      Rake::Task["migration:eraitem"].invoke('spec/fixtures/migration/test-metadata/standard-metadata')
-      result = ActiveFedora::SolrService.instance.conn.get "select", params: {q:["fedora3uuid_tesim:uuid:394266f0-0e4a-42e6-a199-158165226426"]}
-      doc = result["response"]["docs"].first
-      id = doc["id"]
-      @file = GenericFile.find(id)
     end
-    after do
-      Rake::Task["migration:eraitem"].reenable
-      @file.delete
-      @collection.delete
-      @community.delete
+    let!(:item2) do
+      GenericFile.new.tap do |r|
+        r.title = ['Should Not Find This Title']
+        r.apply_depositor_metadata 'jilluser@example.com'
+        r.fedora3uuid = 'uuid:abcdefgh-0123-4567-8901-158165226426' 
+        r.read_groups = ['public']
+        r.save!
+      end
     end
     it "finds uuid" do
       search("uuid:394266f0-0e4a-42e6-a199-158165226426")
-      expect(page).to have_content('Bison sculpture at the entrance to the USGS Ice Core Lab')
+      expect(page).to have_content(item1.title.first)
+      expect(page).to_not have_content(item2.title.first)
     end
   end
 
