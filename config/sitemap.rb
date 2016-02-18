9a6f290797dcdf5621b357cf36fc26116bf3869f
Peter Binkley <peter.binkley@ualberta.ca>
2016-02-17 17:33:54 -0700

    Remove catalog and users from sitemap; add sitemap to .gitignore

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,5 @@
 Sitemap::Generator.instance.load(host: 'era.library.ualberta.ca') do
   path :root, priority: 1, change_frequency: 'weekly'
-  path :catalog_index, priority: 1, change_frequency: 'weekly'
-  User.all.each do |user|
-    literal Sufia::Engine.routes.url_helpers.profile_path(user.to_param), priority: 0.8, change_frequency: 'daily'
-  end
   read_group = Solrizer.solr_name('read_access_group', :symbol)
   solr_rsp = ActiveFedora::SolrService.instance.conn.get "select", :params => {:q => Solrizer.solr_name('read_access_group', :symbol)+':public', :fl => "id,#{Solrizer.solr_name('active_fedora_model', :stored_sortable)}"} 
   numFound = solr_rsp['response']['numFound']
