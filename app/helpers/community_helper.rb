246fe6be5feb98721ae599227e46139df575b8c4
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-02-25 14:19:46 -0700

    performance improvement by avoiding Collection.find #960

--- app/helpers/community_helper.rb
+++ app/helpers/community_helper.rb
@@ -1,6 +1,12 @@
 module CommunityHelper
   def collections_in_community(community_id)
-    collections = Collection.find(community_id).members.to_a
+    query = {
+        fq: "(-hasCollectionId_ssim:[* TO *] AND (belongsToCommunity_ssim:#{community_id} OR belongsToCommunity_tesim:#{community_id})) OR hasCollectionId_ssim:#{community_id}",
+        rows: 1000
+      }
+
+    response = Blacklight.default_index.search(query)
+    response.documents
   end
   def count_collections_in_community(community_id)
     return Collection.find(community_id).member_count
