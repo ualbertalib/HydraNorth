e6f9004660ec40d981a1a66493d9bc5fc341e31b
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-03-01 15:23:13 -0700

    refactor to share filter collection logic with generic_file and batch #887

--- app/controllers/concerns/hydranorth/collections/selects_collections.rb
+++ app/controllers/concerns/hydranorth/collections/selects_collections.rb
@@ -52,4 +52,20 @@ module Hydranorth::Collections::SelectsCollections
       d1.title <=> d2.title
     end
   end
+
+private
+  # Sorting by title implemented in hydra-collections v7.0.0 [projecthydra/hydra-collections@e8e57e5] this is a workaround
+  def find_filtered_collections_sorted(access_level = nil)
+    # need to know the user if there is an access level applied otherwise we are just doing public collections
+    authenticate_user! unless access_level.blank?
+
+    # run the solr query to find the collections
+    query = collections_search_builder(access_level).with({q: "#{Solrizer.solr_name('belongsToCommunity')}:#{params[:community_id]}"}).query
+    response = repository.search(query)
+    # return the user's collections (or public collections if no access_level is applied)
+
+   response.documents.sort do |d1, d2|
+     d1.title <=> d2.title
+   end 
+  end
 end
