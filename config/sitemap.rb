1efdbda909c70aa8d17bf31501e6edf44e58930b
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-05-06 16:39:39 -0600

    add rs:ln information about content link for correctness  fixes #1056

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -10,7 +10,7 @@ Sitemap::Generator.instance.load(host: 'era.library.ualberta.ca') do
     case o[Solrizer.solr_name('active_fedora_model', :stored_sortable)]
     when 'GenericFile'
       gf = GenericFile.find(o['id'])
-      literal Sufia::Engine.routes.url_helpers.generic_file_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)], metadata: { hash: gf.characterization.digest.first.to_s, length: gf.file_size.first, type: gf.mime_type }
+      literal Sufia::Engine.routes.url_helpers.generic_file_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)], metadata: { type: "text/html" }, link: { href: Sufia::Engine.routes.url_helpers.download_path(o['id']), rel: 'content', hash: gf.characterization.digest.first.to_s, length: gf.file_size.first, type: gf.mime_type }
     when 'Collection'
       literal Hydra::Collections::Engine.routes.url_helpers.collection_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)]
     end
