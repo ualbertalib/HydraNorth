c855ae3dc15f94003ec1dd062bb77af2ae8b56ab
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-06-21 17:09:18 -0600

    add error handling to sitemap solves #1195

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -9,8 +9,13 @@ Sitemap::Generator.instance.load(host: 'era.library.ualberta.ca') do
   solr_rsp['response']['docs'].each do |o|
     case o[Solrizer.solr_name('active_fedora_model', :stored_sortable)]
     when 'GenericFile'
-      gf = GenericFile.find(o['id'])
-      literal Sufia::Engine.routes.url_helpers.generic_file_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)], metadata: { type: "text/html" }, link: { href: Sufia::Engine.routes.url_helpers.download_path(o['id']), rel: 'content', hash: gf.characterization.digest.first.to_s, length: gf.file_size.first, type: gf.mime_type }
+      begin
+        gf = GenericFile.find(o['id'])
+      rescue => e
+        Rails.logger.error "id:#{o['id']} threw '#{e}' and it was not included in the sitemap.xml"
+      else
+        literal Sufia::Engine.routes.url_helpers.generic_file_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)], metadata: { type: "text/html" }, link: { href: Sufia::Engine.routes.url_helpers.download_path(o['id']), rel: 'content', hash: gf.characterization.digest.first.to_s, length: gf.file_size.first, type: gf.mime_type }
+      end
     when 'Collection'
       literal Hydra::Collections::Engine.routes.url_helpers.collection_path(o['id']), priority: 1, change_frequency: 'weekly', updated_at: o[Solrizer.solr_name('system_modified', :stored_sortable, type: :date)]
     end
