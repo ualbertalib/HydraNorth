8f16518fd4488450b939c3064f466469d088aac6
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-08-17 10:56:03 -0600

    additional resque config change to initialize with proper redis setup (#1386)
    modified redis/resque configuration files and use secrets.yml to initialize redis instance

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,12 +1,2 @@
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-# use config/redis.yml to load settings
-redis_config_path = "#{rails_root}/config/redis.yml"
-redis_config = YAML.load(ERB.new(IO.read(redis_config_path)).result)
-
-# initialize redis connection
-Resque.redis = redis_config[rails_env]
-
-# pull out the parsed redis driver from resque
-$redis = Resque.redis.instance_eval{@redis}
+require 'redis'
+Redis.current = Redis.new(url: ENV['REDIS_URL'] || Rails.application.secrets.redis_url)
