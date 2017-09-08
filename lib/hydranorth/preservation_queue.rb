b2f3ee0a9b38760d96f55571162d1fc991a80d0c
Weiwei Shi <weiwei.shi@ualberta.ca>
2017-09-08 14:05:44 -0600

    remove unnecessary error handling in pushing to preservation queue

--- lib/hydranorth/preservation_queue.rb
+++ lib/hydranorth/preservation_queue.rb
@@ -11,7 +11,6 @@ module Hydranorth::PreservationQueue
   def preserve(noid)
     queue.with do |conn|
       status = conn.zadd QUEUE_NAME, Time.now.to_f, noid
-      raise PreservationError unless status == true
     end
     # rescue all preservation errors so that the user can continue to use the application normally
   rescue StandardError => e
